65410716818111d093fe5268714a22f6f86d4648
sabitzhan <sgagpgpg@gmail.com>
2026-01-17 10:38:57 +0500

    asik 3, added method to fill second table(realtor)

--- dbms.java
+++ dbms.java
@@ -41,4 +41,40 @@ public class dbms {
             }
         }
     }
+
+    public static void addRealtor(int id, String name, String agency, String property) {
+        String connectionURL = "jdbc:postgresql://localhost:5432/postgres";
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            con = DriverManager.getConnection(connectionURL, "postgres", "0000");
+
+            String sqlProperty = "INSERT INTO realtor (id, name, agency, property) VALUES (?, ?, ?, ?) ";
+            stmt = con.prepareStatement(sqlProperty);
+
+            stmt.setInt(1, id);
+            stmt.setString(2, name);
+            stmt.setString(3, agency);
+            stmt.setString(4, property);
+            stmt.executeUpdate();
+
+            System.out.println("realtor " + name + " added");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver not found.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (con != null) con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
